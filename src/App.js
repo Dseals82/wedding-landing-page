72320b05807cf11c6e281b2951460d108982cd56
Darius Seals <tristarmediagroupinc@gmail.com>
2019-05-01 23:25:08 -0400

    added icons and some styling

--- src/App.js
+++ src/App.js
@@ -20,42 +20,47 @@ function App() {
           <li className="nav-item">
             <a className="nav-link" href="#">Pricing</a>
           </li>
-          <li className="nav-item">
-            <a className="nav-link disabled" href="#">Disabled</a>
-          </li>
         </ul>
       </div>
     </nav>
       <div className="jumbotron jumbotron-fluid ">
-
+        <h1 className="main-text">Some Text Goes Here</h1>
+        <div className="container">
+          <h4 className="main-text-two">
+            "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
+          </h4>
+        </div>
       </div>
       <div className="card container front-card">
-      <h3 id="card-text">Some Text</h3>
+      <h3 id="card-text">Capture The Moment</h3>
       <div className="container">
         <div className="row">
           <div className="col-sm">
-            <div class="mb-3" >
-                <div class="card-body">
-                  <h5 class="card-title">Light card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div className="mb-3" >
+                <div className="card-body">
+                <i class="fas fa-heart"></i>
+                  <h5 className="card-title">Light card title</h5>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
               </div>
            </div>
           <div className="col-sm">
-          <div class="mb-3" >
+          <div className="mb-3" >
 
-              <div class="card-body">
-                <h5 class="card-title">Light card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <div className="card-body">
+              <i class="fas fa-heart"></i>
+                <h5 className="card-title">Light card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
               </div>
             </div>
           </div>
           <div className="col-sm">
-          <div class="mb-3" >
+          <div className="mb-3" >
 
-              <div class="card-body">
-                <h5 class="card-title">Light card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <div className="card-body">
+              <i class="fas fa-heart"></i>
+                <h5 className="card-title">Light card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
               </div>
             </div>
           </div>
